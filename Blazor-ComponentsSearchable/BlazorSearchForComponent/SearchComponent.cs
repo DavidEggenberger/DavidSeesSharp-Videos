400e0524df09542599a605560f6c367766280bff
David Eggenberger <72417712+DannyAinge44@users.noreply.github.com>
2021-03-04 21:43:52 +0100

    Add Blazor-ComponentsSearchable

---
+++ Blazor-ComponentsSearchable/BlazorSearchForComponent/SearchComponent.cs
@@ -0,0 +1,27 @@
+﻿using BlazorSearchForComponent.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSearchForComponent
+{
+    public class SearchComponent : ComponentBase
+    {
+        public string[] Tags { get; set; }
+        [CascadingParameter]
+        public MainLayout MainLayout { get; set; }
+        public bool IsSearchAfter
+        {
+            get
+            {
+                if(MainLayout.searchWord.Length > 3)
+                {
+                    return Tags.Any(t => t.ToUpper().Contains(MainLayout.searchWord.ToUpper()));
+                }
+                return false;
+            }
+        }
+    }
+}
