a205853f46096c0f65468416edd00f86f73ad3ba
David Eggenberger <72417712+DavidEggenberger@users.noreply.github.com>
2021-03-08 15:14:25 +0100

    Add Blazor-ServerEFCore5

---
+++ Blazor-ServerEFCore5/BlazorServerEFCore5/Migrations/20201201201404_initial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorServerEFCore5.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PageVisits",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TimeOfVisit = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PageVisits", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PageVisits");
+        }
+    }
+}
---
+++ Blazor-ServerEFCore5/BlazorServerEFCore5/PageVisit.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerEFCore5
+{
+    public class PageVisit
+    {
+        public Guid Id { get; set; }
+        public DateTime TimeOfVisit { get; set; }
+    }
+    public class VisitContext : DbContext
+    {
+        public DbSet<PageVisit> PageVisits { get; set; }
+        public VisitContext(DbContextOptions opt) : base(opt)
+        {
+
+        }
+    }
+}
