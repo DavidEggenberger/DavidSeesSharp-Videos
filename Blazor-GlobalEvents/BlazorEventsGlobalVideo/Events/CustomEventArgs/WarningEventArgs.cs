38ef78050a9db473bf8541489531b35ef6b7f615
David Eggenberger <72417712+DavidEggenberger@users.noreply.github.com>
2021-04-28 23:30:08 +0200

    Add files via upload

---
+++ Blazor-GlobalEvents/BlazorEventsGlobalVideo/Events/CustomEventArgs/WarningEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEventsGlobalVideo.Events.CustomEventArgs
+{
+    public class WarningEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+        public DateTime Date { get; } = DateTime.Now;
+    }
+}
---
+++ Blazor-GlobalEvents/BlazorEventsGlobalVideo/Events/EventBase.cs
@@ -0,0 +1,23 @@
+﻿using BlazorEventsGlobalVideo.Events.CustomEventArgs;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEventsGlobalVideo.Events
+{
+    public class EventBase : ComponentBase
+    {
+        public static event EventHandler<WarningEventArgs> WarningTriggered;
+        public static event EventHandler WarningResseted;
+        public void TriggerWarning(string message)
+        {
+            WarningTriggered?.Invoke(this, new WarningEventArgs { Message = message });
+        }
+        public void ResetWarning()
+        {
+            WarningResseted?.Invoke(this, null);
+        }
+    }
+}
