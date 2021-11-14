f9369d46123141b39cfbe36c37c1126c8beea118
David Eggenberger <72417712+DavidEggenberger@users.noreply.github.com>
2021-11-14 21:51:48 +0100

    Add files via upload

---
+++ Blazor-ServerHttpContextAccessing/BlazorServerIHttpContextAccessorVideo/Cookie.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerIHttpContextAccessorVideo
+{
+    public class Cookie
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+    }
+}
