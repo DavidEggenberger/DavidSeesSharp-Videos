b0437ee97105dd4af4cc53c575dbf8685500240c
David Eggenberger <72417712+DavidEggenberger@users.noreply.github.com>
2021-11-15 19:43:12 +0100

    Add files via upload

---
+++ Blazor-ServerSignalRIdentityChat/BlazorServerSignalRIdentity/Login.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorServerSignalRIdentity
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class Login : ControllerBase
+    {
+        public async Task<ActionResult> LoginMethod([FromForm] string userName)
+        {
+            var user = HttpContext.User;
+            if(user.Claims.Count() > 0)
+            {
+                await HttpContext.SignOutAsync();
+                OurHub.currentUser.Remove(user.Claims.First().Value);
+            }
+            IEnumerable<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, userName)
+            };
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "auth");
+            ClaimsPrincipal cp = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(cp);
+            return Redirect("/");
+        }
+    }
+}
---
+++ Blazor-ServerSignalRIdentityChat/BlazorServerSignalRIdentity/OurHub.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazorServerSignalRIdentity
+{
+    public class OurHub : Hub
+    {
+        public static Dictionary<string, int> currentUser = new Dictionary<string, int>();
+        public async Task NotifyEveryoneIn()
+        {
+            var username = Context.User.Claims.Where(s => s.Type == ClaimTypes.NameIdentifier).FirstOrDefault().Value;
+            if (currentUser.ContainsKey(username))
+            {
+                currentUser[username]++;
+            }
+            else
+            {
+                currentUser.Add(username, 1);
+            }
+            await Clients.All.SendAsync("Update");
+        }
+        public async Task NotifyEveryoneOut()
+        {
+            var username = Context.User.Claims.Where(s => s.Type == ClaimTypes.NameIdentifier).FirstOrDefault().Value;
+            if(currentUser.Where(s => s.Key == username).First().Value == 1)
+            {
+                currentUser.Remove(username);
+            }
+            await Clients.All.SendAsync("Update");
+        }
+        public async Task SendSpecific(string userName, string Message)
+        {
+            var username = Context.User.Claims.Where(s => s.Type == ClaimTypes.NameIdentifier).FirstOrDefault().Value;
+            await Clients.User(userName).SendAsync("ReceiveMessage", username, Message);
+        }
+    }
+}
